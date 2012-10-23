1213d9fdcbdb31ea480ee04ec3dcfbe29575f9dc
kehribar <kehribar@kehribarNetbook.(none)>
2012-10-23 20:51:29 +0300

    initial commit.

---
+++ custom_build_upload/main.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+*
+* Basic blinky example code which doesn't uses Arduino IDE
+*	ihsan Kehribar - 2012
+******************************************************************************/
+#include <avr/io.h>
+#include <avr/delay.h>
+#include "digital.h"
+
+int main()
+{
+	pinMode(B,5,OUTPUT);
+
+	while(1)
+	{
+		digitalWrite(B,5,HIGH);
+		_delay_ms(500);
+		digitalWrite(B,5,LOW);
+		_delay_ms(500);
+
+		/*togglePin(B,5);
+		_delay_ms(500);*/
+	}	
+
+	return 0;
+}
